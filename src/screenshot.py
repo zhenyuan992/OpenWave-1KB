0bfc423159f501f1cbb4a8b382cda141eb914e12
Tom Dietel <tom@dietel.net>
2020-07-17 14:37:39 +0200

    Fix and improve command line utils

--- src/screenshot.py
+++ src/screenshot.py
@@ -1,27 +1,35 @@
 #!/usr/bin/env python3
 
+import matplotlib.pyplot as plt
+
 from gw_com_1kb import com
 from gw_lan import lan
 import dso1kb
 
 #Check interface according to config file or command line argument.
-#port=com.scanComPort()
+# port=com.scanComPort()
 
 #Connecting to a DSO.
 dso=dso1kb.Dso("127.0.0.1:3001")
 
 # for i in range(1,5):
 #     print(i, dso.isChannelOn(i))
-#
+
 # dso.write(":CHAN1:DISP ON\n")
 # dso.write(":CHAN2:DISP ON\n")
 # dso.write(":CHAN3:DISP OFF\n")
 # dso.write(":CHAN4:DISP OFF\n")
+#
+# dso.getRawData(True, 1)
 
-dso.getRawData(True, 1)
-# dso.getRawData(True, 2)
-fwave1 = dso.convertWaveform(1,1)
-# fwave2 = dso.convertWaveform(2,1)
 
-plt.plot(fwave1)
+dso.write(':DISP:OUTP?\n')                 #Send command to get image from DSO.
+
+# dso.write(':DISP:PNGOutput?\n')            #Send command to get image from DSO.
+dso.getBlockData()
+dso.ImageDecode(1)
+# self.showImage()
+# plt.tight_layout(True)
+
+plt.imshow(dso.im)
 plt.show()
